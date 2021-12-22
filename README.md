f7b25773b8d7143c3fbbb04eed242299678428f7
Peyton Hamilton <peterhamilton522@Gmail.com>
2021-12-22 11:12:54 +0000

    Added files

---
+++ README.md
@@ -0,0 +1,12 @@
+# DummyBot 🤖
+
+## What is DummyBot?
+
+This is the main landing page for my DummyBot project; an open source server monitoring and reporting discord bot. With the main goal of providing an accompanying bot to any discord server that utilises game servers as the centre of their community.
+
+
+
+
+## About me 🏳️‍⚧️
+
+I am a girl who's a fulltime student, along with being a developer interested in creating software that I enjoy working on. Helping to further my learning in different coding languages and APIs.
---
+++ SRC/DummyBot/Program.cs
@@ -0,0 +1,104 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+
+
+
+
+namespace DummyBot
+{
+    class Program
+    {
+        public string serverstat_delta1_bedrock;
+        public string serverstat_delta1_jellyfin;
+
+
+        public static void Main(string[] args)
+        => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient client;
+        public async Task MainAsync()
+        {
+            client = new DiscordSocketClient();
+            client.MessageReceived += CommandHandler;
+            client.Log += Log;
+
+
+            var token = File.ReadAllText("Token.txt");
+
+
+
+
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private Task CommandHandler(SocketMessage message)
+        {
+            string command = "";
+            int lengthOfCommand = -1;
+
+
+            if (!message.Content.StartsWith("!"))
+                return Task.CompletedTask;
+
+            if (message.Author.IsBot)
+                return Task.CompletedTask;
+
+            if (message.Content.Contains(' '))
+                lengthOfCommand = message.Content.IndexOf(' ');
+            else
+                lengthOfCommand = message.Content.Length;
+
+            command = message.Content.Substring(1, lengthOfCommand - 1).ToLower();
+
+
+            if (command.Equals("jellyfin"))
+            {
+                ServerStatusCheck();
+
+                message.Channel.SendMessageAsync(message.Author.Mention + "Pinging Servers... " + "\n" + serverstat_delta1_jellyfin);
+
+
+
+            }
+
+
+            return Task.CompletedTask;
+        }
+
+        public void ServerStatusCheck()
+        {
+            TcpClient tcpClient = new TcpClient();
+
+            try
+            {
+                tcpClient.Connect("86.14.65.74", 8096);
+
+                serverstat_delta1_jellyfin = "Jellyfin@Deltaserver1: Online";
+
+            }
+            catch (Exception)
+            {
+                serverstat_delta1_jellyfin = "Jellyfin@Deltaserver1: Offline";
+            }
+        }
+    }
+}
+
